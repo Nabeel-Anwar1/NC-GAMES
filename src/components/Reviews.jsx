32858ac15b44286e4b2040a4d992d19ab7fccd86
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2022-11-01 11:20:59 +0000

    working dropdown sorter for category

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,26 +1,43 @@
 import { useEffect, useState } from "react"
 import { fetchReviews } from "./api"
+import { Link, useParams } from "react-router-dom";
 
 const Reviews = () => {
     const [isLoading, setIsLoading] = useState(true)
     const [reviews, setReviews] = useState([])
+    const [category, setCategory] = useState("")
 
     useEffect(()=>{
         setIsLoading(true)
-        fetchReviews().then((review) => {
+        fetchReviews(category).then((review) => {
             setReviews(review)
             setIsLoading(false)
         })
-    }, [])
+    }, [category])
 
     if (isLoading) return <h3>Loading...</h3>
     return <div className="reviewContent">
+        <div className="dropdown">
+        <label htmlFor="category">View Category:</label>
+            <select onChange={(event) => {setCategory(event.target.value)}} name="category" id="category">
+                <option value="">Select a category:</option>
+                <option value="">All</option>
+                <option value="hidden-roles">Hidden Roles</option>
+                <option value="dexterity">Dexterity</option>
+                <option value="push-your-luck">Push Your Luck</option>
+                <option value="roll-and-write">Roll and Write</option>
+                <option value="deck-building">Deck Building</option>
+                <option value="engine-building">Engine Building</option>
+            </select>
+        </div>
+
         <ul className="reviewsList">
             {reviews.map((review) => {
             return <div key={review.review_id} className="individualReview">
                 {<li >
                     <b><u>Review By: {review.owner} </u></b> <br />
                     <b><u>Title:</u></b> {review.title}    <br />
+                    <b><u>Category:</u></b> {review.category} <br />
                     <b><u>Comments:</u></b> {review.comment_count}  <br />
                     <b><u>Votes:</u></b> {review.votes} <br />
                     {new Date(review.created_at).toLocaleDateString('en-gb',
--- src/components/api.js
+++ src/components/api.js
@@ -4,8 +4,14 @@ const gamesApi = axios.create({
   baseURL: "https://nabeelgameproject.herokuapp.com/api",
 });
 
-export const fetchReviews = () => {
-  return gamesApi.get("/reviews").then((response) => {
-    return response.data.reviews;
-  });
+export const fetchReviews = (query) => {
+  if (query === "") {
+    return gamesApi.get(`/reviews`).then((response) => {
+      return response.data.reviews;
+    });
+  } else {
+    return gamesApi.get(`/reviews?category=${query}`).then((response) => {
+      return response.data.reviews;
+    });
+  }
 };
