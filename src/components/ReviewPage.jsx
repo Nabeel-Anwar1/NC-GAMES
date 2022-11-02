369efcdc6cfd3988a6c755efed19357870f085b1
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2022-11-02 10:07:51 +0000

    single review page made, css on image and text align

--- src/components/ReviewPage.jsx
+++ src/components/ReviewPage.jsx
@@ -1,5 +1,36 @@
+import { useState } from "react"
+import { useParams } from "react-router-dom"
+import { fetchSingleReview } from "../api"
+
 const ReviewPage = () => {
-    return <h3>Hello</h3>
+    const [review, setReview] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
+    const {review_id} = useParams()
+
+    useState(() => {
+        setIsLoading(true)
+        fetchSingleReview(review_id).then((data)=>{
+            setReview(data)
+            setIsLoading(false)
+        })
+    }, [review_id])
+
+    return isLoading ?  <h3>Loading...</h3> :
+    <section className="singleReviewPage">
+        <h2>Review for: {review.title}</h2>
+        <img src={`${review.review_img_url}`} alt={`${review.title}`} className="reviewImg"></img>
+        <p>{review.review_body}</p>
+        <p>Written By: {review.owner}</p>
+        <p>Created: {new Date(review.created_at).toLocaleDateString('en-gb',
+                        {
+                            year: 'numeric',
+                            month: 'long',
+                            day: 'numeric'
+                        }
+                    )} </p>
+        <p>Votes: {review.votes}</p>
+        <p>Comments: {review.comment_count} </p>
+    </section>
 }
 
 export default ReviewPage
