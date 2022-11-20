6fdacd75644f72efe75d3e48df009d2bd11de550
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2022-11-20 13:45:42 +0000

    style homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom"
 
 const Home = () => {
     return <div>
-    <Link to="/reviews"><p>Reviews</p></Link>
+    <Link to="/reviews"><p className="reviewhome">Have a look at all the reviews here!</p></Link>
     </div>
 }
 
