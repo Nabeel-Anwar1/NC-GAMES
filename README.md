d8edae4d2032d9176f3155390192a87d54dfe1a9
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2023-09-30 17:13:36 +0100

    readme updated, database loading message added

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>Welcome to NC Games by Nabeel.<h1> 
+
+This project was made using React, it includes a backend where I seeded my data on ElephantSQL and then hosted it on Render.
+
+The frontend makes api calls to the database and is rendered on the website hosted on Netlify. 
+
+I am using the free version of Render, this means that the instance will spin down when inactive. Therefore, when the site is loaded for the first time after some time of inactivity, it may take a while to show the data as the database is spinning up.
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,55 +1,80 @@
-import {Link} from "react-router-dom"
-import { useEffect, useState } from "react"
-import { fetchReviews } from "../api"
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { fetchReviews } from "../api";
 
 const Home = () => {
-    const [reviews, setReviews] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-    const sortBy = "votes"
-    const category = "all"
-    const orderBy = "desc"
+  const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const sortBy = "votes";
+  const category = "all";
+  const orderBy = "desc";
 
-    useEffect(()=>{
-        setIsLoading(true)
-        fetchReviews(category, sortBy, orderBy).then((review) => {
-            setReviews(review)
-            setIsLoading(false)
-        })
-    }, [])
+  useEffect(() => {
+    setIsLoading(true);
+    fetchReviews(category, sortBy, orderBy).then((review) => {
+      setReviews(review);
+      setIsLoading(false);
+    });
+  }, []);
 
-    return (
-        isLoading ? 
-        <div>
-            <h3 className="load">Loading...</h3>
-        </div>
-        :
+  return isLoading ? (
     <div>
-        <p className="feature">Featured Reviews!</p>
-        <section>
-        <ul className="reviewsList">
-            {reviews.slice(0,3).map((review) => {
-            return <div key={review.review_id} className="individualReview">
-                {<Link to={`/reviews/${review.review_id}` }><li className="list-items">
-                    <b><u>Review By: {review.owner} </u></b> <br />
-                    <b><u>Title:</u></b> {review.title}    <br />
-                    <b><u>Category:</u></b> {review.category} <br />
-                    <b><u>Comments:</u></b> {review.comment_count}  <br />
-                    <b><u>Votes:</u></b> {review.votes} <br />
-                    {new Date(review.created_at).toLocaleDateString('en-gb',
-                        {
-                            year: 'numeric',
-                            month: 'long',
-                            day: 'numeric'
-                        }
-                    )}
-                </li></Link>}
-            </div>
-            })}
-        </ul>
-    </section>
-    <Link to="/reviews"><u><p className="reviewhome">Have a look at all the reviews here!</p></u></Link>
+      <h3 className="load">Loading...</h3>
+      <h4 className="load">
+        The database is spinning up! This may take a few minutes.
+      </h4>
     </div>
-    )
-}
+  ) : (
+    <div>
+      <p className="feature">Featured Reviews!</p>
+      <section>
+        <ul className="reviewsList">
+          {reviews.slice(0, 3).map((review) => {
+            return (
+              <div key={review.review_id} className="individualReview">
+                {
+                  <Link to={`/reviews/${review.review_id}`}>
+                    <li className="list-items">
+                      <b>
+                        <u>Review By: {review.owner} </u>
+                      </b>{" "}
+                      <br />
+                      <b>
+                        <u>Title:</u>
+                      </b>{" "}
+                      {review.title} <br />
+                      <b>
+                        <u>Category:</u>
+                      </b>{" "}
+                      {review.category} <br />
+                      <b>
+                        <u>Comments:</u>
+                      </b>{" "}
+                      {review.comment_count} <br />
+                      <b>
+                        <u>Votes:</u>
+                      </b>{" "}
+                      {review.votes} <br />
+                      {new Date(review.created_at).toLocaleDateString("en-gb", {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                      })}
+                    </li>
+                  </Link>
+                }
+              </div>
+            );
+          })}
+        </ul>
+      </section>
+      <Link to="/reviews">
+        <u>
+          <p className="reviewhome">Have a look at all the reviews here!</p>
+        </u>
+      </Link>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
